b60bb3d5e18e151f559945a40c4f18f9be699746
hoalongnatsu <hoalongnatsu@gmail.com>
2022-05-26 17:32:54 +0700

    add bai 7

---
+++ bai-7/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	persons := [3]string{"Max", "Alex", "Tom"}
+
+	fmt.Println("Number of secret person 0: Max, 1: Alex, 2: Tom.")
+
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	secretPersonNumber := r.Intn(3)
+
+	var answer int
+	var err error
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Printf("Please enter your answer with numer 0 - 2: ")
+		scanner.Scan()
+		input := scanner.Text()
+		answer, err = strconv.Atoi(input)
+
+		if err != nil {
+			log.Panic(err)
+		}
+
+		if answer >= 0 && answer <= 2 {
+			break
+		}
+	}
+
+	if answer == secretPersonNumber {
+		fmt.Printf("Congratulations!! You answer is correct, %s is the secret person.\n", persons[answer])
+	} else {
+		fmt.Printf("Sorry!! You answer is incorrect, %d: %s is the secret person.\n", secretPersonNumber, persons[answer])
+	}
+}
